4f72300bbb73fc16f0c6eacb80b18d2e313cabf7
John (Jack) Meyer <jack10042@gmail.com>
2020-03-29 00:56:43 -0400

    Bump major versions, update domain, content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faAward, faLightbulb, faFlask } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faYoutube, faChrome } from '@fortawesome/free-brands-svg-icons';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faAward, faLightbulb, faFlask } from '@fortawesome/free-solid-svg-icons';
-import { faGithub, faYoutube, faChrome } from '@fortawesome/free-brands-svg-icons';
 import { HomeComponent } from './home/home.component';
 import { ResumeViewComponent } from './resume-view/resume-view.component';
-import { ProjectItemComponent } from './project/project-item/project-item.component';
 import { HomeItemComponent } from './home-item/home-item.component';
 
 @NgModule({
@@ -25,15 +23,15 @@ import { HomeItemComponent } from './home-item/home-item.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule.forRoot(),
+    NgbModule,
     FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor() {
+  constructor(library: FaIconLibrary) {
     // Add an icon to the library for convenient access in other components
-    library.add(faAward, faLightbulb, faFlask, faGithub, faYoutube, faChrome);
+    library.addIcons(faAward, faLightbulb, faFlask, faGithub, faYoutube, faChrome);
   }
 }
